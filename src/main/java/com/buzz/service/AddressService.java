95fc1dde81362eec942c63eebc46d79ddacdc403
evgenkras <krasnenkov.evgen@gmail.com>
2022-06-11 17:28:40 -0400

    "initial commit"

---
+++ src/main/java/com/buzz/service/AddressService.java
@@ -0,0 +1,34 @@
+package com.buzz.service;
+
+import com.buzz.dto.AddressResponse;
+import com.buzz.dto.CreateAddressRequest;
+import com.buzz.model.Address;
+import com.buzz.repos.AddressRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+    Logger logger = LoggerFactory.getLogger(AddressService.class);
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public AddressResponse createAddress(CreateAddressRequest createAddressRequest) {
+        Address address = new Address();
+        address.setCity(createAddressRequest.getCity());
+        address.setStreet(createAddressRequest.getStreet());
+        Address savedAddress = addressRepository.save(address);
+        return new AddressResponse(savedAddress);
+    }
+
+    public AddressResponse getById(long id) {
+        logger.info("AddressService provides:: getById with ID - {}", id);
+        boolean present = addressRepository.findById(id).isPresent();
+        AddressResponse addressResponse = new AddressResponse(addressRepository.findById(id).orElseThrow());
+        return addressResponse;
+    }
+}
